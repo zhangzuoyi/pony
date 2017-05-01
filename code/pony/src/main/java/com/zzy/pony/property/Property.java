6e5eea6b43f8e9808e7eba4667bb0af08b624be5
zhangzuoyi <zhangzuoyi@zhangzuoyi-PC>
2017-05-01 22:10:36 +0800

    增加资产管理实体

---
+++ code/pony/src/main/java/com/zzy/pony/property/Property.java
@@ -0,0 +1,265 @@
+package com.zzy.pony.property;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.zzy.pony.model.User;
+
+
+/**
+ * The persistent class for the t_property database table.
+ * 
+ */
+@Entity
+@Table(name="t_property")
+@NamedQuery(name="Property.findAll", query="SELECT p FROM Property p")
+public class Property implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="PROP_ID")
+	private int propId;
+
+	private String brand;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="BUY_DATE")
+	private Date buyDate;
+
+	private String comments;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="CREATE_TIME")
+	private Date createTime;
+
+	@Column(name="CREATE_USER")
+	private String createUser;
+
+	private String description;
+
+	private String location;
+
+	private String name;
+
+	private Float price;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="PRODUCT_DATE")
+	private Date productDate;
+
+	@Column(name="PROP_CODE")
+	private String propCode;
+
+	private String spec;
+
+	private String status;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="UPDATE_TIME")
+	private Date updateTime;
+
+	@Column(name="UPDATE_USER")
+	private String updateUser;
+
+	//bi-directional many-to-one association to PropertyType
+	@ManyToOne
+	@JoinColumn(name="TYPE_ID")
+	private PropertyType propertyType;
+
+	//bi-directional many-to-one association to Department
+	@ManyToOne
+	@JoinColumn(name="DEPT_ID")
+	private Department department;
+
+	//bi-directional many-to-one association to User
+	@ManyToOne
+	@JoinColumn(name="OWNER_ID")
+	private User owner;
+
+	//bi-directional many-to-one association to User
+	@ManyToOne
+	@JoinColumn(name="USER_ID")
+	private User user;
+
+	//bi-directional many-to-one association to PropertyTrace
+//	@OneToMany(mappedBy="property")
+//	private List<PropertyTrace> traces;
+
+	public Property() {
+	}
+
+	public int getPropId() {
+		return this.propId;
+	}
+
+	public void setPropId(int propId) {
+		this.propId = propId;
+	}
+
+	public String getBrand() {
+		return this.brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public Date getBuyDate() {
+		return this.buyDate;
+	}
+
+	public void setBuyDate(Date buyDate) {
+		this.buyDate = buyDate;
+	}
+
+	public String getComments() {
+		return this.comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public Date getCreateTime() {
+		return this.createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getCreateUser() {
+		return this.createUser;
+	}
+
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLocation() {
+		return this.location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Float getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+
+	public Date getProductDate() {
+		return this.productDate;
+	}
+
+	public void setProductDate(Date productDate) {
+		this.productDate = productDate;
+	}
+
+	public String getPropCode() {
+		return this.propCode;
+	}
+
+	public void setPropCode(String propCode) {
+		this.propCode = propCode;
+	}
+
+	public String getSpec() {
+		return this.spec;
+	}
+
+	public void setSpec(String spec) {
+		this.spec = spec;
+	}
+
+	public String getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getUpdateTime() {
+		return this.updateTime;
+	}
+
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+
+	public String getUpdateUser() {
+		return this.updateUser;
+	}
+
+	public void setUpdateUser(String updateUser) {
+		this.updateUser = updateUser;
+	}
+
+	public PropertyType getPropertyType() {
+		return this.propertyType;
+	}
+
+	public void setPropertyType(PropertyType propertyType) {
+		this.propertyType = propertyType;
+	}
+
+	public Department getDepartment() {
+		return this.department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public User getOwner() {
+		return owner;
+	}
+
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
