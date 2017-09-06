0bd2af2b10c72739e68ed5cf06839f6977a1139e
zhangzuoyi <zhangzuoyi@163.com>
2017-09-06 09:49:56 +0800

    解决冲突

--- code/pony/src/main/java/com/zzy/pony/exam/mapper/ExamineeMapper.java
+++ code/pony/src/main/java/com/zzy/pony/exam/mapper/ExamineeMapper.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.zzy.pony.exam.mapper;
 
 import java.util.List;
@@ -24,29 +23,3 @@ public interface ExamineeMapper {
 	void insertExamineeArrangeByExaminee(int arrangeId,int examineeId);
 	void deleteExamineeArrangeByArrangeId(int arrangeId);
 }
-=======
-package com.zzy.pony.exam.mapper;
-
-import java.util.List;
-
-import com.zzy.pony.vo.RankVo;
-import org.apache.ibatis.annotations.Param;
-
-import com.zzy.pony.exam.model.Examinee;
-import com.zzy.pony.exam.vo.ExamineeVo;
-
-public interface ExamineeMapper {
-	List<ExamineeVo> findPageByClass(int offset,int pageSize, int examId, int classId);
-	int findCountByClass(int examId, int classId);
-	List<ExamineeVo> findPageByClassAndArrange(int offset,int pageSize, int examId, int classId,int arrangeId);
-	int findCountByClassAndArrange(int examId, int classId,int arrangeId);
-	List<Examinee> findByExamIdAndClassId(int examId,int classId);
-	List<Examinee> findByArrangeId(int arrangeId);
-	List<Examinee> findByExamIdAndClassIds(int examId,@Param(value="classIds")int[] classIds);
-	List<RankVo> findRankByExam(int examId);
-	void deleteExamineeArrangeByClass(int examId,int arrangeId,@Param(value="classIds")int[] classIds);
-	void insertExamineeArrangeByClass(int examId,int arrangeId,@Param(value="classIds")int[] classIds);
-	void insertExamineeArrangeByExaminee(int arrangeId,int examineeId);
-	void deleteExamineeArrangeByArrangeId(int arrangeId);
-}
->>>>>>> refs/remotes/origin/master
